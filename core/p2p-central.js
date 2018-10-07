80639b7fd69b24fd0b4147d02230061a5d1350e0
JCloudYu <j.cloud.yu@gmail.com>
2018-10-07 15:09:46 +0800

    1. Completely rewrite p2p-central's logic 2. Remove redundant library

--- core/p2p-central.js
+++ core/p2p-central.js
@@ -1,68 +1,79 @@
-(() => {
-    'use strict';
-
-    require('../lib/customize');
-
-    module.exports = (pemu) => {
-
-        let isInit = false;
-        let corePrepared;
-        const corePromise = new Promise((resolve) => { corePrepared = resolve });
-
-        pemu
-        .on('tasks-ready', () => {
-            if (isInit) return;
-
-            pemu.local('__p2p-central-identification');
-            isInit = true;
-            corePrepared();
-        })
-        .on('net-group-attach', (e) => {
-            pemu.send(e.sender, '__p2p-central-identification');
-            corePrepared();
-        })
-        .on('net-group-detach', async (e) => {
-            let nodeIds = await Object.callMethod(pemu, 'nodeGroupDetach', e.sender);
-            if (!Array.isArray(nodeIds)) {
-                nodeIds = ((nodeIds) ? [nodeIds] : []);
-            }
-
-            for (let nodeId of nodeIds) {
-                pemu.emit('__p2p-node-disconnect', nodeId);
-            }
-        })
-        .on('__p2p-node-connect', async (e) => {
-            if (!e.respondWith) return;
-
-            // get neighbors and response
-            let neighbors = await Object.callMethod(pemu, 'nodeConnect', e.sender);
-            e.respondWith(neighbors);
-
-            for (let nodeId of neighbors) {
-                pemu.send(nodeId, 'p2p-update-neighbors');
-            }
-        })
-        .on('__p2p-node-disconnect', (e, ...args) => {
-            Object.callMethod(pemu, 'nodeDisconnect', ...args);
-        })
-        .on('__p2p-fetch-neighbors', async (e) => {
-            if (!e.respondWith) return;
-
-            // get neighbors and response
-            let neighbors = await Object.callMethod(pemu, 'fetchNeighbors', e.sender);
-            e.respondWith(neighbors);
-        });
-
-        let module = {
-            initP2PEnv: (cb) => {
-                if (typeof cb !== 'function') {
-                    return corePromise;
-                }
-    
-                return Promise.all([Promise.resolve(cb()), corePromise]);
-            }
-        };
-
-        return module;
-    } 
+(()=>{
+	'use strict';
+	
+	
+	
+	module.exports = (pemu)=>{
+		const RUNTIME = {
+			_initialized: false
+		};
+		
+		
+		
+		let corePrepared;
+		const corePromise = new Promise((resolve)=>{ corePrepared = resolve });
+		Object.defineProperties(pemu, {
+			initP2PEnv: {
+				value: (cb)=>{
+					if( typeof cb !== 'function' ){
+						return corePromise;
+					}
+					
+					return Promise.all([Promise.resolve(cb()), corePromise]);
+				},
+				enumerable: true
+			}
+		});
+		
+		
+		
+		pemu
+		.on('tasks-ready', ()=>{
+			if( RUNTIME._initialized ) return;
+			
+			pemu.local('__p2p-central-identification');
+			RUNTIME._initialized = true;
+			corePrepared();
+		})
+		.on('net-group-attach', (e)=>{
+			pemu.send(e.sender, '__p2p-central-identification');
+			corePrepared();
+		})
+		.on('net-group-detach', async (e)=>{
+			if( typeof pemu.nodeGroupDetach !== "function" ) return;
+			
+			let nodeIds = await pemu.nodeGroupDetach(e.sender);
+			if( !Array.isArray(nodeIds) ){
+				nodeIds = ((nodeIds) ? [nodeIds] : []);
+			}
+			
+			for( let nodeId of nodeIds ){
+				pemu.emit('__p2p-node-disconnect', nodeId);
+			}
+		})
+		.on('__p2p-node-connect', async (e)=>{
+			if( !e.respondWith ) return;
+			if( typeof pemu.nodeConnect !== "function" ) return;
+			
+			// get neighbors and response
+			let neighbors = await pemu.nodeConnect(e.sender);
+			e.respondWith(neighbors);
+			
+			for( let nodeId of neighbors ){
+				pemu.send(nodeId, 'p2p-update-neighbors');
+			}
+		})
+		.on('__p2p-node-disconnect', (e, ...args)=>{
+			if( typeof pemu.nodeDisconnect !== "function" ) return;
+			pemu.nodeDisconnect(...args);
+		})
+		.on('__p2p-fetch-neighbors', async (e)=>{
+			if( !e.respondWith ) return;
+			if( typeof pemu.fetchNeighbors !== "function" ) return;
+			
+			// get neighbors and response
+			let neighbors = await pemu.fetchNeighbors(e.sender);
+			e.respondWith(neighbors);
+		});
+	}
 })();
--- index.js
+++ index.js
@@ -1,39 +1,12 @@
 (()=>{
 	'use strict';
 	
-	require('./lib/customize');
 	
-	let coreList = ['central', 'node'];
 	
-	module.exports.expand = (pemu, coreName = 'node')=>{
-		if( (coreList.indexOf(coreName) === -1) || !pemu ) return;
+	const CORE_NAME_LIST = [ 'node', 'central' ];
+	module.exports.expand = (pemu, coreName = CORE_NAME_LIST[0])=>{
+		if( (CORE_NAME_LIST.indexOf(coreName) === -1) || !pemu ) return;
 		
-		let props = {}, methods = {};
-		const core = require(`./core/p2p-${coreName}`)(pemu);
-		if ( !core ) return;
-		
-		
-		
-		for( let prop in core ){
-			if( !core.hasOwnProperty(prop) ) continue;
-			
-			// classify methods
-			if( typeof(core[prop]) === 'function' ){
-				methods[prop] = core[prop]
-			}
-			else{
-				props[prop] = core[prop];
-			}
-		}
-		
-		// assign property
-		for( let prop in props ){
-			if( !props.hasOwnProperty(prop) ) continue;
-			
-			pemu[prop] = props[prop];
-		}
-		
-		// assign method
-		Object.setConstant(pemu, methods);
+		require(`./core/p2p-${coreName}`)(pemu);
 	};
 })();
